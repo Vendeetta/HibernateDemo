2f5d3bf90371d7e1a0028bbd4d936916078a661c
Vendeetta <vendeetta@gmail.com>
2023-04-27 11:51:15 +0300

    HW

---
+++ src/main/java/gerasimov/model/CreditCard.java
@@ -0,0 +1,103 @@
+package gerasimov.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
+
+@Entity
+@Table(name = "credit_card")
+public class CreditCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "bank_name")
+    private String bankName;
+
+    @Column(name = "number")
+    private int number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+
+    public CreditCard() {
+    }
+
+    public CreditCard(Integer id, String bankName, int number, User user) {
+        this.id = id;
+        this.bankName = bankName;
+        this.number = number;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        CreditCard that = (CreditCard) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "id=" + id +
+                ", bankName='" + bankName + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
